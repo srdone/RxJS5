50eeae459bd9c751c93af06158f605106120e672
Stephen Done <sdone@simplifile.com>
2017-05-25 17:25:54 -0600

    adding a retry strategy using scan, takeWhile, and delay operators

--- main.ts
+++ main.ts
@@ -10,14 +10,31 @@ function load(url: string) {
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener("load", () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         })
 
         xhr.open("GET", url);
         xhr.send();
     })
+    .retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function (errors : Observable<any>) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
 }
 
 function renderMovies(movies) {
@@ -28,10 +45,12 @@ function renderMovies(movies) {
     });
 }
 
-load("movies.json").subscribe(renderMovies);
+//load("movies.json").subscribe(renderMovies);
 
 click
-    .flatMap(e => load("movies.json"))
+    .flatMap(e => load("moviess.json"))
     .subscribe(
-        renderMovies
+    renderMovies,
+    e => console.error(`error: ${e}`),
+    () => console.log("complete")
     )
