a7363e84ad42707a7639c438a3b711ff8680f841
Stephen Done <sdone@simplifile.com>
2017-05-25 17:14:34 -0600

    using flatmap, cold observables

--- main.ts
+++ main.ts
@@ -6,21 +6,32 @@ let button = document.getElementById('button');
 let click = Observable.fromEvent(button, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
-        });
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        })
+
+        xhr.open("GET", url);
+        xhr.send();
     })
-
-    xhr.open("GET", url);
-    xhr.send();
 }
 
-click.subscribe(
-    e => load("movies.json")
-)
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+load("movies.json").subscribe(renderMovies);
+
+click
+    .flatMap(e => load("movies.json"))
+    .subscribe(
+        renderMovies
+    )
