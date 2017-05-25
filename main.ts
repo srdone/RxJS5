403b14547eb3e0d991c0d3c670e518a91e397bf8
Stephen Done <sdone@simplifile.com>
2017-05-25 17:05:10 -0600

    handle button events and xhr requests - removed extra code for circle following

--- main.ts
+++ main.ts
@@ -1,20 +1,9 @@
 import { Observable } from 'rxjs';
 
-let circle = document.getElementById("circle");
 let output = document.getElementById('output');
 let button = document.getElementById('button');
 
-let mouseMoves = Observable.fromEvent(document, 'mousemove')
-    .map((e : MouseEvent) => ({x: e.clientX, y: e.clientY}))
-    .filter(value => value.x < 500)
-    .delay(300);
-
 let click = Observable.fromEvent(button, "click");
-
-function onNext (value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
-}
 
 function load(url: string) {
     let xhr = new XMLHttpRequest();
@@ -31,12 +20,6 @@ function load(url: string) {
     xhr.open("GET", url);
     xhr.send();
 }
-
-mouseMoves.subscribe(
-        onNext,
-        e => console.log(`error: ${e}`),
-        () => console.log('complete')
-    )
 
 click.subscribe(
     e => load("movies.json")
