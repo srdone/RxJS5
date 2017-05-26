c3d43d75a0ab1ba5584654a69b90b06694e7ca3f
Stephen Done <sdone@simplifile.com>
2017-05-26 07:24:12 -0600

    onErrorResumeNext and catch operators

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 import { Observable } from 'rxjs';
 import { load, loadWithFetch } from "./loader";
 
-let source = Observable.create(observer => {
-    observer.next(1);
-    observer.next(2);
-    // observer.error("Stop!");
-    throw new Error("Stop!");
-    // observer.next(3);
-    // observer.complete();
-});
+let source = Observable.merge( //or onErrorResumeNext
+    Observable.of(1),
+    Observable.from([2,3,4]),
+    Observable.throw(new Error("Stop!")),
+    Observable.of(5)
+).catch(e => {
+    console.log(`caught: ${e}`);
+    return Observable.of(10);
+})
 
 source.subscribe(
     value => console.log(`value: ${value}`),
